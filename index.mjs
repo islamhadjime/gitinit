b11a939330f91c36b931b0905f07171832c390d1
Amir <57444044+islamhadjime@users.noreply.github.com>
2025-01-16 18:37:08 +0300

    v2

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
 import { process } from './process.mjs';
 
-// Тестовые данные
 const store = [
   { size: 2, quantity: 4 },
 ];
@@ -10,8 +9,7 @@ const order = [
   { id: 102, size: [1, 2], masterSize: 's2' },
 ];
 
-// Запуск функции
+
 const result = process(store, order);
 
-// Вывод результата
 console.log(result);
